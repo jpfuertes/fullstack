9c8734399aa2e8e911e9ba383b367870c5892261
Juan Pablo <juanpablo@Juans-MacBook-Pro.local>
2016-11-20 19:02:25 -0300

    output to dist

--- gruntfile.js
+++ gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
               "!src/_all.d.ts",
               "!node_modules/**/*.ts"
               ],
-          dest: "."
+          dest: "dist"
         }],
         options: {
           fast: 'never',
